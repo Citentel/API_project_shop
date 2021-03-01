163b4b39fa361d701388ced0b83e69bbb218fe27
Citentel <gontrebor@gmail.com>
2021-03-01 21:08:55 +0100

    added delivery entity

---
+++ src/Entity/Delivery.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DeliveryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DeliveryRepository::class)
+ */
+class Delivery
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $priceCrossed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPriceCrossed(): ?int
+    {
+        return $this->priceCrossed;
+    }
+
+    public function setPriceCrossed(?int $priceCrossed): self
+    {
+        $this->priceCrossed = $priceCrossed;
+
+        return $this;
+    }
+}
